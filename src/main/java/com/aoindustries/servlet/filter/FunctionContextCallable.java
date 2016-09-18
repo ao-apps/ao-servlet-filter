ea708c8a12310fc2f1e6d8b9f3a692b852d7eaf5
AO Industries, Inc. <support@aoindustries.com>
2016-09-18 02:27:03 -0500

    Now automatically maintains some ThreadLocal values, and providers wrapper hook for subclasses to extend.

---
+++ src/main/java/com/aoindustries/servlet/filter/FunctionContextCallable.java
@@ -0,0 +1,42 @@
+/*
+ * ao-servlet-filter - Reusable Java library of servlet filters.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-servlet-filter.
+ *
+ * ao-servlet-filter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-servlet-filter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-servlet-filter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.servlet.filter;
+
+import com.aoindustries.util.concurrent.ThreadLocalsCallable;
+import java.util.concurrent.Callable;
+
+/**
+ * Maintains current function context for the provided callable.
+ */
+public class FunctionContextCallable<T> extends ThreadLocalsCallable<T> {
+
+	static final ThreadLocal[] threadLocals = {
+		FunctionContext.servletContextTL,
+		FunctionContext.requestTL,
+		FunctionContext.responseTL
+	};
+
+	public FunctionContextCallable(Callable<T> task) {
+		super(task, threadLocals);
+	}
+}
